0226b635db466592329c2c8d85a8d93483f172c0
Matt Wrock <matt@mattwrock.com>
2012-10-27 18:38:57 -0700

    redirecting tortoisegit to alanstevens repo

--- TortoiseGit/tools/ChocolateyInstall.ps1
+++
@@ -1,1 +0,0 @@
-Install-ChocolateyPackage 'TortoiseGit (Latest)' 'MSI' '/QN /norestart REBOOT=ReallySuppress' 'http://tortoisegit.googlecode.com/files/TortoiseGit-1.7.14.0-32bit.msi' 'http://tortoisegit.googlecode.com/files/TortoiseGit-1.7.14.0-64bit.msi'
